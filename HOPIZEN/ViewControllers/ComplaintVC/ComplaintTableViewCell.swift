e24e3d030888a4222deeb6f15a31e55fa3f6f280
Hung Hoang <hiepkhach9x@Hungs-iMac.local>
2017-05-20 19:44:29 -0700

    create design

---
+++ HOPIZEN/ViewControllers/ComplaintVC/ComplaintTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ComplaintTableViewCell.swift
+//  PublicEyes
+//
+//  Created by Hung Hoang on 5/20/17.
+//  Copyright © 2017 Hoang Ngoc Hung. All rights reserved.
+//
+
+import UIKit
+
+class ComplaintTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ HOPIZEN/ViewControllers/LocationVC/PostSuccessView.swift
@@ -0,0 +1,21 @@
+//
+//  PostSuccessView.swift
+//  PublicEyes
+//
+//  Created by Hung Hoang on 5/20/17.
+//  Copyright © 2017 Hoang Ngoc Hung. All rights reserved.
+//
+
+import UIKit
+
+class PostSuccessView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
