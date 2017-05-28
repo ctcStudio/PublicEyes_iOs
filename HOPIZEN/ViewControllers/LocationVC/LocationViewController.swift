89c8a71d883404d53ab6a9b4e4a8e9190c62c6df
Hung Hoang <hiepkhach9x@Hungs-iMac.local>
2017-05-28 02:36:05 -0700

    get current address - update complaint

--- HOPIZEN/ViewControllers/LocationVC/LocationViewController.swift
+++ HOPIZEN/ViewControllers/LocationVC/LocationViewController.swift
@@ -7,21 +7,26 @@
 //
 
 import UIKit
+import CoreLocation
 
 class LocationViewController: UIViewController, CLLocationManagerDelegate {
     
     var path:String!
     var des: String!
     var category:CategoryModel!
+    var locationStr:String?
+    var district:String?
+    var province:String?
     
+    @IBOutlet weak var tvLocation: UITextField!
     let locationManager = CLLocationManager()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -33,23 +38,53 @@ class LocationViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
     }
-
+    
     @IBAction func continues(_ sender: Any) {
+        let params = NSMutableDictionary.init();
+        params.setObject(category.name ?? "", forKey: "category_id" as NSCopying)
+        params.setObject(category.id ?? 0,forKey: "category_name" as NSCopying)
+        params.setObject(path ?? "", forKey: "path" as NSCopying)
+        params.setObject(locationStr ?? "",forKey: "location" as NSCopying)
+        params.setObject(district ?? "", forKey: "district" as NSCopying)
+        params.setObject(province ?? "",forKey: "province" as NSCopying)
+        params.setObject(tvLocation.text ?? "",forKey: "address" as NSCopying)
+        params.setObject(userDefault.string(forKey: UserDefault_email) ?? "",forKey: "email" as NSCopying)
+        params.setObject(des ?? "",forKey: "desciption" as NSCopying)
+        //params.setObject(time) ?? "",forKey: "create_date" as NSCopying)
+        
+        HPZWebservice.shareInstance.updateComplaint(path: API_UPDATE_COMPLAINT, params: params, handler:{success , response in
+            if(success) {
+                if(response?.isKind(of: HPZMessageModel.self))!{
+                    let message:HPZMessageModel = response as! HPZMessageModel
+                    if(message.code == 0) {
+                        return
+                    } else {
+                        let alert = UIAlertController(title: "Alert", message: message.message, preferredStyle: UIAlertControllerStyle.alert)
+                        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+                        self.present(alert, animated: true, completion: nil)
+                    }
+                }
+            }
+            let alert = UIAlertController(title: "Alert", message: "Kết nối server thất bại", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }, entity: HPZMessageModel())
     }
     
     
     func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
-        CLGeocoder().reverseGeocodeLocation(manager.location, completionHandler: {(placemarks, error) -> Void in
-            if error {
-                println("Reverse geocoder failed with error" + error.localizedDescription)
+        CLGeocoder().reverseGeocodeLocation(manager.location!, completionHandler: {(placemarks, error) -> Void in
+            if (error != nil) {
+                print("Reverse geocoder failed with error" + (error?.localizedDescription)!)
                 return
             }
             
-            if placemarks.count > 0 {
-                let pm = placemarks[0] as CLPlacemark
-                self.displayLocationInfo(pm)
+            if (placemarks?.count)! > 0 {
+                self.locationStr = String.init(manager.location!.coordinate.latitude) + "," + String.init(manager.location!.coordinate.longitude)
+                let pm = placemarks?.last
+                self.displayLocationInfo(placemark: pm!)
             } else {
-                println("Problem with the data received from geocoder")
+                print("Problem with the data received from geocoder")
             }
         })
     }
@@ -83,32 +118,43 @@ class LocationViewController: UIViewController, CLLocationManagerDelegate {
                 if(state.isEmpty == false) {
                     address.append(state)
                 }
-                self.addressView.text = address.joined(separator: ", ")
+                self.tvLocation.text = address.joined(separator: ", ")
             }
                 
             else {
                 print("Problem with the data received from geocoder")
             }
-            
-            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                self.hasGetAddress = false
-            }
         })
     }
-
+    
     
     func displayLocationInfo(placemark: CLPlacemark) {
-        if placemark != nil {
-            //stop updating location to save battery life
-            locationManager.stopUpdatingLocation()
-            println(placemark.locality ? placemark.locality : "")
-            println(placemark.postalCode ? placemark.postalCode : "")
-            println(placemark.administrativeArea ? placemark.administrativeArea : "")
-            println(placemark.country ? placemark.country : "")
+        //stop updating location to save battery life
+        locationManager.stopUpdatingLocation()
+        let addressDic = placemark.addressDictionary
+        var address:[String] = []
+        let subAdministrativeArea = addressDic?["SubAdministrativeArea"] as? String ?? ""
+        let name = addressDic?["Name"] as? String ?? ""
+        let street = addressDic?["Street"] as? String ?? ""
+        let state = addressDic?["State"] as? String ?? ""
+        if(subAdministrativeArea.isEmpty == false) {
+            address.append(subAdministrativeArea)
         }
+        if(name.isEmpty == false) {
+            address.append(name)
+        }
+        if(street.isEmpty == false) {
+            self.district = street
+            address.append(street)
+        }
+        if(state.isEmpty == false) {
+            self.province = state
+            address.append(state)
+        }
+        self.tvLocation.text = address.joined(separator: ", ")
     }
     
-    func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!) {
-    println("Error while updating location " + error.localizedDescription)
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error while updating location " + error.localizedDescription)
     }
 }
